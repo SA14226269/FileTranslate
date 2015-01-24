a17df1105b631f26786a00254e4763b7a195050c
SA14226269 <924045318@qq.com>
2015-01-24 14:00:52 +0800

    tcp translate version 1.0

--- client.c
+++ client.c
@@ -4,65 +4,53 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<string.h>
+#include<stdio.h>
+#include<time.h>
 
-#define SERVER_PORT 5005
- struct filepage{
-    int num;
-    char buf[1024];
-};
+#define SERVER_PORT 5000
+ 
 int main()
-{
-	int clientfd;
+{       
+        time_t t_start,t_end;
+        int sockfd;
+        struct sockaddr_in server_addr;             
+        sockfd = socket(AF_INET,SOCK_STREAM,0);
+		char sendBuf[1024] = "\0";
+		char recvBuf[1024] = "\0";
+        bzero(&server_addr,sizeof(struct sockaddr_in));
+        server_addr.sin_family=AF_INET;
+        server_addr.sin_port = htons(SERVER_PORT);
+        inet_aton("127.0.0.1",&server_addr.sin_addr);
 
-	char sendBuf[1024] = "\0";
-	char recvBuf[1024] = "\0";
+ 
+    
+        if(-1 == connect(sockfd,(struct sockaddr *)&server_addr, sizeof(struct sockaddr)))
+		{
+				printf("error connect!");
+               // exit(1);  
+		}
+	
+		recv(sockfd,recvBuf,10,0);
+        printf("%c%c%c%c\n",recvBuf[0],recvBuf[1],recvBuf[2],recvBuf[3]);
         
-	int n;
-	struct sockaddr_in serveraddr;
+		FILE *fd = fopen("hehe.file", "wb+");
+        t_start=time(NULL);
 	
-	clientfd = socket(AF_INET, SOCK_DGRAM, 0);
-	if(clientfd < 0)
-	{
-		printf("error!\n");
-	}
-	bzero(&serveraddr, sizeof(struct sockaddr_in));
-	
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	serveraddr.sin_port = htons(SERVER_PORT);
-	printf("This is a client!\n");
-	
-	int len = sizeof(serveraddr);
-      //测试
-	n = sendto(clientfd, "Hello", 5, 0, (struct sockaddr *)&serveraddr, len);
-	
-
-	FILE *fd = fopen("hehe.txt", "a+");
-	int i = 0;
-	int size = 1024 *1024;
-	struct filepage p;
-	while(i < size)
-        {       
-                char rec[sizeof (struct filepage)];
-                memset(rec,0,sizeof(rec));
-		recvfrom(clientfd, rec, sizeof(rec), 0, (struct sockaddr *)&serveraddr, &len);
-                memset(&p,0,sizeof(struct filepage));
-                memcpy(&p,rec,sizeof(struct filepage));
-                strcpy(recvBuf,p.buf);
-                if(p.num != i)
-               {
-                  printf("error");
-                   sendto(clientfd,&p.num,sizeof(unsigned int),0,(struct sockaddr*)&serveraddr,len);
-              }
-	     //   else
-                fputs(p.buf, fd);
-                
-                printf("********%d\n",p.num);
-		i++;
-		//sendto(clientfd,"i",10,0,(Struct sockaddr*)&serveraddr,len);
-	}
-	fclose(fd);
-	printf("finished\n");
+        while(1)
+		{
+       
+				if(-1 == recv(sockfd,recvBuf,1024,0))
+                break;
+				fwrite(recvBuf,1,1024,fd);
+				//printf("ooo\n");
+				send(sockfd,"ok",2,0);
+          
+        }
+        t_end=time(NULL);
+        printf("共用时%.0fs\n",difftime(t_end,t_start));
+        fclose(fd);
+        
+        close(sockfd);
         return 0;
 }
 
